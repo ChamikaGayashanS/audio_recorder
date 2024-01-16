1ac4f81e0065352c9ee6b4caeacba82204cf06b6
Chamika Gayashan <chamika@infragist.com>
2024-01-16 15:06:19 +0530

    added firebase and ffmpg kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:audio_player/firebase_options.dart';
 import 'package:audio_player/pages/recoder_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
--- lib/pages/recoder_page.dart
+++ lib/pages/recoder_page.dart
@@ -1,19 +1,22 @@
 import 'dart:io';
+import 'dart:typed_data';
+import 'package:audio_player/pages/recorder_controller.dart';
 import 'package:audio_player/widgets/rounded_buton.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
 
-class RecorderPage extends StatefulWidget {
+class RecorderPage extends ConsumerStatefulWidget {
   const RecorderPage({super.key});
 
   @override
-  State<RecorderPage> createState() => _RecorderPageState();
+  ConsumerState<RecorderPage> createState() => _RecorderPageState();
 }
 
-class _RecorderPageState extends State<RecorderPage> {
+class _RecorderPageState extends ConsumerState<RecorderPage> {
   bool recording = false;
   bool playing = false;
   Record recorder = Record();
@@ -52,6 +55,9 @@ class _RecorderPageState extends State<RecorderPage> {
   initializeAudioPlayer() async {
     await audioPlayer.setLoopMode(LoopMode.one);
     await audioPlayer.setAudioSource(AudioSource.file(recorderPath));
+    Duration duration = await audioPlayer.load() ?? const Duration();
+    print(
+        'Duration in Seconds - ${duration.inSeconds} -------------------------------------------------------------------------------------------------');
   }
 
   playPauseAudio() async {
@@ -68,6 +74,7 @@ class _RecorderPageState extends State<RecorderPage> {
 
   @override
   Widget build(BuildContext context) {
+    final controller = ref.read(recorderPageControllerProvider);
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -79,14 +86,21 @@ class _RecorderPageState extends State<RecorderPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // RoundedButton(
-                //   radius: 50,
-                //   icon: const Icon(Icons.mic, color: Colors.white),
-                //   backgroundColor: Colors.red,
-                //   borderColors: Colors.black45,
-                //   onTap: () {},
-                // ),
-                const Gap(70),
+                RoundedButton(
+                  radius: 50,
+                  icon: const Icon(Icons.document_scanner, color: Colors.white),
+                  backgroundColor: Colors.red,
+                  borderColors: Colors.black45,
+                  onTap: () async {
+                    File file = File(recorderPath);
+                    await file.readAsBytes().then((value) {
+                      Uint8List byteArray = value;
+                      print('Byte Array Length - ${byteArray.length}');
+                      print(byteArray);
+                    });
+                  },
+                ),
+                const Gap(40),
                 RoundedButton(
                   radius: 60,
                   icon: Icon(recording ? Icons.stop : Icons.mic,
