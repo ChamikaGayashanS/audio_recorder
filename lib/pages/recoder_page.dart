ae36b1a8cc166f33a213543d516b14168ed2832a
Thushan Isuru <101393829+isurucentronixx@users.noreply.github.com>
2024-02-16 13:03:10 +0530

    fixed

--- lib/pages/recoder_page.dart
+++ lib/pages/recoder_page.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:audio_waveforms/audio_waveforms.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:path_provider/path_provider.dart';
 
 class RecorderPage extends StatefulWidget {
   const RecorderPage({super.key});
@@ -14,64 +17,84 @@ class RecorderPage extends StatefulWidget {
 class _RecorderPageState extends State<RecorderPage> {
   bool recording = false;
   late RecorderController recorderController;
-  String audioPath = '';
+  String path = '';
   AudioPlayer audioPlayer = AudioPlayer();
+  bool isRecording = false;
+  bool isRecordingCompleted = false;
+  bool isLoading = true;
+  late Directory appDirectory;
 
   @override
   void initState() {
-    // TODO: implement initState
-    getSavedAudioPaths();
+    getSavedpaths();
     initRecorder();
   }
 
-  getSavedAudioPaths() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    final path = preferences.getString("audioPath");
-    audioPath = path ?? "";
+  getSavedpaths() async {
+    appDirectory = await getApplicationDocumentsDirectory();
+    path = "${appDirectory.path}/recording.m4a";
+    isLoading = false;
     setState(() {});
   }
 
   initRecorder() {
-    super.initState();
     recorderController = RecorderController()
       ..androidEncoder = AndroidEncoder.aac
       ..androidOutputFormat = AndroidOutputFormat.mpeg4
       ..iosEncoder = IosEncoder.kAudioFormatMPEG4AAC
-      ..sampleRate = 16000;
+      ..sampleRate = 44100;
   }
 
   controlReorder() async {
-    if (recording) {
-      final path = await recorderController.stop();
-      debugPrint(path);
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      await preferences.setString("audioPath", path!);
-      audioPath = path ?? "";
-      recording = false;
-    } else {
-      await recorderController.record();
-      recording = true;
+    try {
+      if (recording) {
+        recorderController.reset();
+
+        final path = await recorderController.stop(false);
+
+        if (path != null) {
+          isRecordingCompleted = true;
+          debugPrint(path);
+          debugPrint("Recorded file size: ${File(path).lengthSync()}");
+        }
+        recording = false;
+      } else {
+        await recorderController.record(path: path!);
+
+        recording = true;
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    } finally {
+      setState(() {
+        isRecording = !isRecording;
+      });
     }
-    setState(() {});
   }
 
   controlAudioPlayer() async {
     if (audioPlayer.playing) {
       await audioPlayer.pause();
     } else {
-      await initAudio(audioPath);
+      await initAudio(path);
     }
   }
 
-  initAudio(String audioPath) async {
+  initAudio(String path) async {
     try {
       await audioPlayer.stop();
       await audioPlayer.setLoopMode(LoopMode.one);
-      await audioPlayer.setAudioSource(AudioSource.file(audioPath));
+      await audioPlayer.setAudioSource(AudioSource.file(path));
       await audioPlayer.play();
     } catch (e) {
       debugPrint(e.toString());
     }
+  }
+
+  @override
+  void dispose() {
+    recorderController.dispose();
+    super.dispose();
   }
 
   @override
@@ -84,7 +107,7 @@ class _RecorderPageState extends State<RecorderPage> {
           recording
               ? const SizedBox()
               : Text(
-                  audioPath.split('/').last,
+                  path.split('/').last,
                   style: const TextStyle(
                       fontSize: 12, fontWeight: FontWeight.w500),
                 ),
@@ -103,7 +126,7 @@ class _RecorderPageState extends State<RecorderPage> {
                     final playerState = snapshot.data;
                     final playing = playerState?.playing;
                     debugPrint(playing.toString());
-                    return (!recording && audioPath.isNotEmpty)
+                    return (!recording && path.isNotEmpty)
                         ? IconButton(
                             onPressed: controlAudioPlayer,
                             icon: playing ?? false
